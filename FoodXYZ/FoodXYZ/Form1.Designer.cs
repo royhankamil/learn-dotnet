cac48eceb8d2fb85afb39585f37b0ca148d3ff63
Inventaris  RPL <royhan1125@gmail.com>
2025-02-17 02:35:30 +0700

    learn awal

---
+++ FoodXYZ/FoodXYZ/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace FoodXYZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.UsernameInput = new System.Windows.Forms.TextBox();
+            this.PasswordInput = new System.Windows.Forms.TextBox();
+            this.LoginForm = new System.Windows.Forms.Button();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.ErrorText = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FoodXYZ.Properties.Resources.Screenshot_2025_02_17_015139;
+            this.pictureBox1.Location = new System.Drawing.Point(277, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(197, 166);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // UsernameInput
+            // 
+            this.UsernameInput.BackColor = System.Drawing.SystemColors.Control;
+            this.UsernameInput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.UsernameInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UsernameInput.Location = new System.Drawing.Point(263, 229);
+            this.UsernameInput.Margin = new System.Windows.Forms.Padding(4);
+            this.UsernameInput.Name = "UsernameInput";
+            this.UsernameInput.Size = new System.Drawing.Size(228, 20);
+            this.UsernameInput.TabIndex = 1;
+            this.UsernameInput.TextChanged += new System.EventHandler(this.UsernameInput_TextChanged);
+            // 
+            // PasswordInput
+            // 
+            this.PasswordInput.BackColor = System.Drawing.SystemColors.Control;
+            this.PasswordInput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.PasswordInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordInput.Location = new System.Drawing.Point(263, 274);
+            this.PasswordInput.Margin = new System.Windows.Forms.Padding(4);
+            this.PasswordInput.Name = "PasswordInput";
+            this.PasswordInput.Size = new System.Drawing.Size(228, 20);
+            this.PasswordInput.TabIndex = 2;
+            this.PasswordInput.UseSystemPasswordChar = true;
+            this.PasswordInput.TextChanged += new System.EventHandler(this.PasswordInput_TextChanged);
+            // 
+            // LoginForm
+            // 
+            this.LoginForm.Location = new System.Drawing.Point(263, 329);
+            this.LoginForm.Name = "LoginForm";
+            this.LoginForm.Size = new System.Drawing.Size(103, 43);
+            this.LoginForm.TabIndex = 3;
+            this.LoginForm.Text = "Login";
+            this.LoginForm.UseVisualStyleBackColor = true;
+            this.LoginForm.Click += new System.EventHandler(this.LoginForm_Click);
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(384, 329);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(103, 43);
+            this.ResetButton.TabIndex = 4;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            // 
+            // ErrorText
+            // 
+            this.ErrorText.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ErrorText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ErrorText.ForeColor = System.Drawing.Color.Red;
+            this.ErrorText.Location = new System.Drawing.Point(199, 386);
+            this.ErrorText.Name = "ErrorText";
+            this.ErrorText.Size = new System.Drawing.Size(336, 36);
+            this.ErrorText.TabIndex = 5;
+            this.ErrorText.Text = "[Error]";
+            this.ErrorText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ErrorText);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.LoginForm);
+            this.Controls.Add(this.PasswordInput);
+            this.Controls.Add(this.UsernameInput);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox UsernameInput;
+        private System.Windows.Forms.TextBox PasswordInput;
+        private System.Windows.Forms.Button LoginForm;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.Label ErrorText;
+    }
+}
+
---
+++ FoodXYZ/FoodXYZ/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FoodXYZ
+{
+    public partial class Form1 : Form
+    {
+        FoodXYZEntities db = new FoodXYZEntities();
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            ErrorText.Text = "";
+        }
+
+        private void LoginForm_Click(object sender, EventArgs e)
+        {
+            ErrorText.Text = "";
+
+            tbl_user user = db.tbl_user.FirstOrDefault(u => u.username.TrimEnd() == UsernameInput.Text);
+
+            if (user != default && user.password.TrimEnd() == PasswordInput.Text)
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("username atau password yang anda masukkan tidak sesuai !", "Login Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                ErrorText.Text = "username atau password yang anda masukkan tidak sesuai !";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UsernameInput_TextChanged(object sender, EventArgs e)
+        {
+            //UsernameInput.Text = (UsernameInput.Text == "") ? "User Name": "";
+        }
+
+        private void PasswordInput_TextChanged(object sender, EventArgs e)
+        {
+
+            //PasswordInput.Text = (PasswordInput.Text == "") ? "Password": "";
+            //PasswordInput.UseSystemPasswordChar = (PasswordInput.Text != "");
+        }
+    }
+}
